58af694e51184e3fa38f839e51c15b02db50f9d5
Ben Allison <ben@checkfront.com>
2018-02-16 17:33:30 -0800

    Make snake auto reload itself with code changes

--- app/main.py
+++ app/main.py
@@ -285,6 +285,6 @@ def end():
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 if __name__ == '__main__':
-	bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', server_Port))
+	bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', server_Port), reloader=True)
 
 
